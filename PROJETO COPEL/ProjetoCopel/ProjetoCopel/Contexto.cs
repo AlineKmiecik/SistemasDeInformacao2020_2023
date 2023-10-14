d3c5d72edebe35aa6ed8d69b2319acdbff644f19
AlineKmiecik <alinekmiecik@hotmail.com>
2023-10-13 22:34:11 -0300

    Projeto copel

---
+++ PROJETO COPEL/ProjetoCopel/ProjetoCopel/Contexto.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca
+{
+    //Serve para fazer os relacionamentos com o DB
+    class Contexto : DbContext
+    {
+        public DbSet<UnidadeConsumidora> UnidadeConsumidora { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
+        public DbSet<Fatura> Faturas { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+        //conexão do contexto supracitado ao DB
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //OptionBuilder -> p/ mapear o banco desejado (usa a string de conexão com o banco)
+            //Quem é o servidor e qual o nome do banco que gerará
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;
+	                Database=ProjetoCopel; MultipleActiveResultSets=True;
+                    Integrated Security=True");
+        }
+    }
+}
---
+++ PROJETO COPEL/ProjetoCopel/ProjetoCopel/Migrations/20200527010819_InitialMigration.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoCopel.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    IdEndereco = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.IdEndereco);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Faturas",
+                columns: table => new
+                {
+                    IdFatura = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faturas", x => x.IdFatura);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UnidadeConsumidora",
+                columns: table => new
+                {
+                    IdUnidadeConsumidora = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnidadeConsumidora", x => x.IdUnidadeConsumidora);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.IdUsuario);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Endereco");
+
+            migrationBuilder.DropTable(
+                name: "Faturas");
+
+            migrationBuilder.DropTable(
+                name: "UnidadeConsumidora");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
